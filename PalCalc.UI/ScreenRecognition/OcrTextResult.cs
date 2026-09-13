c72dc5e290333a40bd86c8cd919766ca21b32584
sungy940422-cyber <sungy940422@gmail.com>
2026-09-14 07:37:42 +0900

    Add PalCalc.UI/ScreenRecognition/OcrTextResult.cs

---
+++ PalCalc.UI/ScreenRecognition/OcrTextResult.cs
@@ -0,0 +1,8 @@
+namespace PalCalc.UI.ScreenRecognition
+{
+    public sealed class OcrTextResult
+    {
+        public string Text { get; init; }
+        public double Confidence { get; init; }
+    }
+}
