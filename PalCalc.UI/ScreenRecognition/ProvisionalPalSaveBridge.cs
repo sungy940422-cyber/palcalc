1aa5f637ca99146c06b8e87ebc0cff074c7923af
sungy940422-cyber <sungy940422@gmail.com>
2026-09-14 07:45:44 +0900

    Fix Windows build for PalCalc.UI/ScreenRecognition/ProvisionalPalSaveBridge.cs

--- PalCalc.UI/ScreenRecognition/ProvisionalPalSaveBridge.cs
+++ PalCalc.UI/ScreenRecognition/ProvisionalPalSaveBridge.cs
@@ -7,6 +7,7 @@ using PalCalc.UI.ViewModel.PalDerived;
 using PalCalc.UI.ViewModel.SaveSelection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
